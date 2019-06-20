34204e987ac4a65abcfe8de05b2a1f928020ac50
earlyr1 <earlyr@yandex.ru>
2019-06-20 16:11:45 +0300

    updated testing script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-go run server.go & > logs.txt
+go run server.go &
 sleep 1
 curl -X POST -d "url=https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv" localhost:8000/submit
 curl -X GET localhost:8000/check?id=1449244776ba2d5ea034dbc6a4b4b413
