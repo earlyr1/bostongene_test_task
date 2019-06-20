b183cae2e5f1384c8687b5459112c92d3b3043d8
earlyr1 <earlyr@yandex.ru>
2019-06-20 16:10:53 +0300

    added testing script

---
+++ test.sh
@@ -0,0 +1,7 @@
+go run server.go & > logs.txt
+sleep 1
+curl -X POST -d "url=https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv" localhost:8000/submit
+curl -X GET localhost:8000/check?id=1449244776ba2d5ea034dbc6a4b4b413
+curl -X GET localhost:8000/check?id=randomstuff
+curl -X POST -d "url=randomstuff" localhost:8000/submit
+curl -X GET localhost:8000/check?id=c52f05387cdaf678809f8083ac04a7d5
